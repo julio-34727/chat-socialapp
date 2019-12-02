10330415ee266a7088b48c4ee8d32540e08316ca
kibens <j.tusamba@gmail.com>
2019-12-02 16:56:25 +0100

    update all files

--- index.js
+++ index.js
@@ -16,5 +16,17 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
-/* LISTEN */
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+/* SOCKET.io */
+const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+const io = require('socket.io')(server)
+
+io.on('connect', (socket) => {
+    console.log('New user connected (server side')
+
+    socket.on('disconnect', () => {
+        console.log('User disconnect (server side')
+    })
+
+    
+
+})
